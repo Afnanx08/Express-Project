6771ea186d6af285fd3efad9e61fbebe19190707
Afnan Abdullah <afnanjadnan77@.com>
2024-07-31 09:24:43 +0800

    new update

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
+const dotenv = require("dotenv").config();
+
+
 const app = express();
-const port = 5001;
 
+const port = process.env.PORT || 5000;
 
-const contractsRoutes = require('./routes/contractsRoutes');
-
-
-app.use('/api/contacts', contractsRoutes);
+app.use(express.json());
+app.use('/api/contacts', require('./routes/contractsRoutes'));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+  });
